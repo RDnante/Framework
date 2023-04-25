4837008be5fe327c347214cbc5a796febd11c0c3
RDnante <nanterakoto15@gmail.com>
2023-04-25 11:36:11 +0300

    sprint4

---
+++ Script.bat
@@ -0,0 +1,3 @@
+cd "C:\Users\nante\Documents\GitHub\Framework\TestFramework"
+jar cvf "BuildMineframe.war" *
+copy BuildMineFrame.war "C:\Program Files\Apache Software Foundation\Tomcat 9.0\webapps\"
