f368e3e9e66ae3882a203a8390df9cde36890b59
RDnante <nanterakoto15@gmail.com>
2023-04-25 10:38:13 +0300

    sprint2

--- Framework/src/etu1825/framework/servlet/FrontServlet.java
+++ Framework/src/etu1825/framework/servlet/FrontServlet.java
@@ -1,7 +1,12 @@
 package etu1825.framework.servlet;
 
+import java.util.HashMap;
+
 import javax.servlet.http.HttpServlet;
 
+import etu1825.framework.Mapping;
+
 public class FrontServlet extends HttpServlet {
+    HashMap<String,Mapping> MappingUrls = new HashMap<String, Mapping>();
     
 }
